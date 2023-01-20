60bc03e9c8366828c2d53807ae08636725e92ecd
shady <nitesh.poudel@hotmail.com>
2023-01-21 02:39:04 +0545

    gitshelldeployment

--- git.sh
+++ git.sh
@@ -9,7 +9,13 @@ On_Red='\033[41m'
 
 
 init_repo() {
-    if ! git rev-parse --git-dir > /dev/null 2>&1; then
+
+      git_global_config=$(git config --global user.name)
+    if [ -z "$git_global_config" ]; then
+        git config --global user.name "$(whoami)"
+     
+    elif ! git rev-parse --git-dir > /dev/null 2>&1 and ; then
+
         git init
     fi
 }
@@ -24,18 +30,22 @@ add_remote() {
 }
 
 check_project(){
+    if [ -f "docker-compose.yml" ]; then
+        docker-compose up --build
 
-    if [ -f "package.json" ]; then
+    elif [ -f "package.json" ]; then
         echo "This is a Node.js project."
+        npm install
         npm run dev
 
     elif [ -f "manage.py" ]; then
-        echo "This is a Django project."
-        python manage.py migrate
-        python manage.py run server
           if [ -f "requirements.txt" ]; then
         pip install -r requirements.txt
         fi
+        echo "This is a Django project."
+        python manage.py migrate
+        python manage.py run server
+        
 
     elif [ -f "composer.json" ]; then
         echo "This is a PHP project."
@@ -47,6 +57,7 @@ check_project(){
 
     elif jq '.dependencies | has("@angular/core")' package.json; then
     echo "This is an Angular project."
+    npm i
     ng serve
 
     else
@@ -66,10 +77,12 @@ pull_repo(){
         echo "There were merge conflicts. Please resolve them before committing."
         git status
     else
-        git add .
+     git add .
+        system_username=$(whoami)
+        current_date=$(date +"%d/%m/%Y %T")
         echo Type Your Commit message:
         read varname
-        git commit -m "${red}$varname"
+        git commit -m "${red}$varname, by $system_username on $current_date"
         check_project
 
     fi
@@ -77,13 +90,15 @@ pull_repo(){
 
 push_repo(){
     git add .
+    system_username=$(whoami)
+    current_date=$(date +"%d/%m/%Y %T")
     echo Type Your Commit message:
     read varname
     git commit -m "$varname"
     echo Type Your Branch:
     read Branch
 
-    git push origin $Branch
+    git commit -m "${red}$varname, by $system_username on $current_date"
     echo "Git push completed "
 
 
@@ -107,7 +122,7 @@ echo = "
 [13;10H         |_|_|_|_| I I I I I |_|_|_|_|
 [14;10H         | O  O  | | | | | | |  O  O |
 [15;10H         | O  O  | | | | | | |  O  O |
-[16;10H         | O  O  | | | | | | |  O  O |--Work Work Work .
+[16;10H         | O  O  | | | | | | |  O  O |--push push .
 [17;10H         |_______I_I_I_I_I_I_I_______|
 
 [19;10H            [Welcome to git shell ]"
@@ -125,11 +140,21 @@ echo "${red}Do You want to pull or push$:"
 read Code
 
 if [ $Code = pull ]; then
-   
+    echo = "
+[2J
+(B
+[9;10H#6(0       aa
+(B[10;10H                   ____|____
+[11;10H                  /_________\
+[12;10H         ________/_I_I_I_I_I_\________
+[13;10H         |_|_|_|_| I I I I I |_|_|_|_|
+[14;10H         | O  O  | | | | | | |  O  O |
+[15;10H         | O  O  | | | | | | |  O  O |
+[16;10H         | O  O  | | | | | | |  O  O |--push push ."\n
+[17;10H         |_______I_I_I_I_I_I_I_______|
 
-   pull_repo
 
-        toilet -F metal "
+    echo ="
 
 [14;45H   O    
 [15;45H  /|\_ ...........Pulling......
@@ -141,21 +166,43 @@ if [ $Code = pull ]; then
 
     "
 
+   pull_repo
+
+
+
 
 
 elif [ $Code = push ]; then
-    push_repo
-     toilet -F metal "
 
-[14;45H   O    
-[15;45H  /|\_ ...........Pushing......
-[16;45H | |   
+    clear
+
+    echo = "
+[2J
+(B
+[9;10H#6(0       aa
+(B[10;10H                   ____|____
+[11;10H                  /_________\
+[12;10H         ________/_I_I_I_I_I_\________dialog --menu "Select an option:" 0 0 2 1 "push" 2 "pull" 2>/tmp/input.txt
+
+[13;10H         |_|_|_|_| I I I I I |_|_|_|_|
+[14;10H         | O  O  | | | | | | |  O  O |
+[15;10H         | O  O  | | | | | | |  O  O |
+[16;10H         | O  O  | | | | | | |  O  O |--push push .
+[17;10H         |_______I_I_I_I_I_I_I_______|"
+
+    echo "
+
+[14;45H   O      O                
+[15;45H  /|\_   /|\_  ..........Pushing......
+[16;45H | |          
 [17;45H _/ \  
 [18;45H'    |_
 
 
 
     "
+    push_repo
+  
 fi
 
 
